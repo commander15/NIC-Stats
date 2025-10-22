b6703261e04b2eabd894a014b1f14565053a0f6e
commander15 <128611219+commander15@users.noreply.github.com>
2025-10-22 12:43:55 +0100

    Fixed: typo in QXlsx name

--- cmake/FindQxlsx.cmake
+++ cmake/FindQxlsx.cmake
@@ -3,13 +3,9 @@ include(FetchContent)
 FetchContent_Declare(QXlsx
     GIT_REPOSITORY "https://github.com/QtExcel/QXlsx"
     GIT_TAG v1.5.0
-    SOURCE_SUBDIR Qxlsx
+    SOURCE_SUBDIR QXlsx
 )
 
 FetchContent_MakeAvailable(QXlsx)
 
 FetchContent_GetProperties(QXlsx SOURCE_DIR QXLSX_SOURCE_DIR)
-
-if (NOT TARGET QXlsx)
-    add_subdirectory(${QXLSX_SOURCE_DIR}/QXlsx)
-endif()
