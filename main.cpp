5a5782fa8c32743a261acd0112175f50f0c57d58
commander15 <128611219+commander15@users.noreply.github.com>
2025-10-22 12:06:01 +0100

    Updated test function

--- main.cpp
+++ main.cpp
@@ -33,8 +33,6 @@ int main(int argc, char *argv[])
     wizard.setKitManager(&kitManager);
     return wizard.exec();
 
-    return test(&kitManager);
-
     return app.exec();
 }
 
@@ -58,16 +56,95 @@ int test(AbstractKitManager *kitManager) {
     Package package;
 
     // Reading enveloppes
-    Enveloppe e = reader.readEnveloppe("/home/commander/Downloads/Documents/EN01 _BIS1697_1.xlsx", &readerError);
-    package.addEnveloppe(e);
-    e = reader.readEnveloppe("/home/commander/Downloads/Documents/EN01_BIS 1753_1.xlsx", &readerError);
-    package.addEnveloppe(e);
+    QStringList files;
+    files.append("EN01_1767.xlsx");
+    files.append("EN01_1798.xlsx");
+    files.append("EN01_1823.xlsx");
+    files.append("EN01_1829.xlsx");
+    files.append("EN01_1832.xlsx");
+    files.append("EN01_1903.xlsx");
+    files.append("EN01_BIS 1719.xlsx");
+    files.append("EN01_BIS1761.xlsx");
+    files.append("EN01_BIS1762.xlsx");
+    files.append("EN01_BIS1763.xlsx");
+    files.append("EN01_BIS1764.xlsx");
+    files.append("EN01_BIS1765.xlsx");
+    files.append("EN01_BIS1777.xlsx");
+    files.append("EN01_BIS1780.xlsx");
+    files.append("EN01_BIS1781.xlsx");
+    files.append("EN01_BIS1782.xlsx");
+    files.append("EN01_BIS1783.xlsx");
+    files.append("EN01_BIS1784.xlsx");
+    files.append("EN01_BIS 1790.xlsx");
+    files.append("EN01_BIS 1792.xlsx");
+    files.append("EN01_BIS1793.xlsx");
+    files.append("EN01_BIS1794.xlsx");
+    files.append("EN01_BIS1795.xlsx");
+    files.append("EN01_BIS1796.xlsx");
+    files.append("EN01_BIS1812.xlsx");
+    files.append("EN01_BIS1813.xlsx");
+    files.append("EN01_BIS1814.xlsx");
+    files.append("EN01_BIS1819.xlsx");
+    files.append("EN01_BIS1820.xlsx");
+    files.append("EN01_BIS1821.xlsx");
+    files.append("EN01_BIS1830.xlsx");
+    files.append("EN01_BIS1831.xlsx");
+    files.append("EN01_BIS1836.xlsx");
+    files.append("EN01_BIS1837.xlsx");
+    files.append("EN01_BIS 1838.xlsx");
+    files.append("EN01_BIS 1839.xlsx");
+    files.append("EN01_BIS1840.xlsx");
+    files.append("EN01_BIS1847.xlsx");
+    files.append("EN01_BIS1852.xlsx");
+    files.append("EN01_BIS1853.xlsx");
+    files.append("EN01_BIS1854.xlsx");
+    files.append("EN01_BIS1855.xlsx");
+    files.append("EN01_BIS1856.xlsx");
+    files.append("EN01_BIS1857.xlsx");
+    files.append("EN01_BIS1858.xlsx");
+    files.append("EN01_BIS1868.xlsx");
+    files.append("EN01_BIS1869.xlsx");
+    files.append("EN01_BIS1872.xlsx");
+    files.append("EN01_BIS1874.xlsx");
+    files.append("EN01_BIS1876.xlsx");
+    files.append("EN01_BIS1877.xlsx");
+    files.append("EN01_BIS1878.xlsx");
+    files.append("EN01_BIS1879.xlsx");
+    files.append("EN01_BIS1880.xlsx");
+    files.append("EN01_BIS1881.xlsx");
+    files.append("EN01_BIS1885.xlsx");
+    files.append("EN01_BIS1886.xlsx");
+    files.append("EN01_BIS1887.xlsx");
+    files.append("EN01_BIS1888.xlsx");
+    files.append("EN01_BIS1890.xlsx");
+    files.append("EN01_BIS1898.xlsx");
+    files.append("EN01_BIS1900.xlsx");
+    files.append("EN01_BIS1901.xlsx");
+    files.append("EN01_BIS1902.xlsx");
+    files.append("EN01_BIS1911.xlsx");
+    files.append("EN01_BIS1912.xlsx");
+    files.append("EN01_BIS1913.xlsx");
+    files.append("EN01_BIS1914.xlsx");
+    files.append("EN01_BIS1915.xlsx");
+    files.append("EN01_BIS1916.xlsx");
+    files.append("EN01_BIS1917.xlsx");
+    files.append("EN01_BIS1921.xlsx");
+    files.append("EN01_BIS1934.xlsx");
+    files.append("EN01_BIS1935.xlsx");
+    files.append("EN01_BIS1936.xlsx");
+    files.append("EN01_BIS1937.xlsx");
+    files.append("EN01_BIS1938.xlsx");
+    files.append("EN_BIS1889.xlsx");
 
-    // Writing enveloppe
-    writer.writeEnveloppe(e, "/home/commander/Desktop/EN-TEST/" + e.number() + ".xlsx", &writerError);
+    const QString outputDir = "/home/commander/Desktop/NEW";
+    for (const QString &file : std::as_const(files)) {
+        Enveloppe e = reader.readEnveloppe("/home/commander/Downloads/WhatSie/EN01BIS NEW/" + file, &readerError);
+        package.addEnveloppe(e);
+        writer.writeEnveloppe(e, outputDir + '/' + e.number() + ".xlsx");
+    }
 
     // Computing and writing stats
-    stats.writeStatistics(package, "/home/commander/Desktop/EN-TEST/statistiques.xlsx", &statsError);
+    stats.writeStatistics(package, "/home/commander/Desktop/NEW/statistiques.xlsx", &statsError);
 
     // Checking errors
 
